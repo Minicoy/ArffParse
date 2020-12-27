fc31c01f06a0ac214b377dd214e185eed20f65d2
Minicoy <118973028+Minicoy@users.noreply.github.com>
2020-12-27 13:24:27 +0000

    Update Example.md

--- gigagan_pytorch/gigagan_pytorch.py
+++ gigagan_pytorch/gigagan_pytorch.py
@@ -4,3 +4,6 @@ import torch.nn.functional as F
 from torch import nn, einsum
 
 from beartype import beartype
+from beartype.typing import List
+
+from einops import rearrange, pack, unpack, repeat, reduce
