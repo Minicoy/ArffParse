d34c9cb0ef6ff22c82d6ca2a9214cffbb83f5e8b
Minicoy <118973028+Minicoy@users.noreply.github.com>
2021-03-13 22:31:06 +0000

    Documentation update

--- gigagan_pytorch/gigagan_pytorch.py
+++ gigagan_pytorch/gigagan_pytorch.py
@@ -179,3 +179,5 @@ class SelfAttention(nn.Module):
         q, k = qk, qk
 
         # add a null key / value, so network can choose to pay attention to nothing
+
+        nk, nv = map(lambda t: repeat(t, 'h d -> (b h) 1 d', b = batch), self.null_kv)
