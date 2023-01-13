ebee7e9e992b14ce31fe42b86ae4514c0146d042
Minicoy <118973028+Minicoy@users.noreply.github.com>
2023-01-13 10:07:56 +0000

    spi: spi-pxa2xx: Check status register to determine if SSSR_TINT is disabled

--- setup.py
+++ setup.py
@@ -25,4 +25,4 @@ setup(
   classifiers=[
     'Development Status :: 4 - Beta',
     'Intended Audience :: Developers',
-    'Topic :: Scientific/Engineering :: A
+    'Topic :: Scientific/Engineering :: Art
