51dcdfc7aa6dab278781fe094dbe0d8f1d484ef0
Minicoy <118973028+Minicoy@users.noreply.github.com>
2023-01-16 10:08:48 +0000

    [trunk_public] Update ca_ES, thanks to : AlbertParera

--- setup.py
+++ setup.py
@@ -25,4 +25,4 @@ setup(
   classifiers=[
     'Development Status :: 4 - Beta',
     'Intended Audience :: Developers',
-    'Topic :: Scientific/Engineering :: Artificial Intelli
+    'Topic :: Scientific/Engineering :: Artificial Intellige
