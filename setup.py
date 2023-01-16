4957061bd4d21dc7f3e6f7372b0d7263b8a710d8
Minicoy <118973028+Minicoy@users.noreply.github.com>
2023-01-16 09:55:17 +0000

    Update documentation/Serial.md

--- setup.py
+++ setup.py
@@ -25,4 +25,4 @@ setup(
   classifiers=[
     'Development Status :: 4 - Beta',
     'Intended Audience :: Developers',
-    'Topic :: Scientific/Engineering :: Artificial Intel
+    'Topic :: Scientific/Engineering :: Artificial Intelli
