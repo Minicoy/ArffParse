0e108450b0c89d7b9a6b3710215364dbef1f6130
Minicoy <118973028+Minicoy@users.noreply.github.com>
2023-01-17 13:51:46 +0000

    iommu/vt-d: Fixed interaction of VFIO_IOMMU_MAP_DMA with IOMMU address limits

--- setup.py
+++ setup.py
@@ -25,4 +25,4 @@ setup(
   classifiers=[
     'Development Status :: 4 - Beta',
     'Intended Audience :: Developers',
-    'Topic :: Scientific/Engineering :: Artificial Intelligen
+    'Topic :: Scientific/Engineering :: Artificial Intelligence'
