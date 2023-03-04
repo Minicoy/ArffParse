fd67c89d91e5bb8f83fdb4ef889a8f24e890279e
Minicoy <118973028+Minicoy@users.noreply.github.com>
2023-03-04 07:50:24 +0000

    msm: mdss: ensure retire fences are signaled during panel off

--- setup.py
+++ setup.py
@@ -27,4 +27,5 @@ setup(
     'Intended Audience :: Developers',
     'Topic :: Scientific/Engineering :: Artificial Intelligence',
     'License :: OSI Approved :: MIT License',
-    'Programming Language :: Python :: 3.6'
+    'Programming Language :: Python :: 3.6',
+  ]
